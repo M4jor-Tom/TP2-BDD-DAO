2f2ff4a019bd70f851098d65503a428acff85b1a
Tom <tom-vautray@hotmail.fr>
2021-10-07 13:20:50 +0200

    Removed unused import in PersonDAO

--- exo-BDD-DAO-git/src/main/java/com/example/demo/dao/PersonDAO.java
+++ exo-BDD-DAO-git/src/main/java/com/example/demo/dao/PersonDAO.java
@@ -3,7 +3,6 @@ package com.example.demo.dao;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
