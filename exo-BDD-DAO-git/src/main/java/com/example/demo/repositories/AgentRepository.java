ffa19637d9bbf58bbd8928aa36384d759ea8b4b3
Tom <tom-vautray@hotmail.fr>
2021-10-07 13:54:41 +0200

    Added specific methods in AgentRepository

--- exo-BDD-DAO-git/src/main/java/com/example/demo/repositories/AgentRepository.java
+++ exo-BDD-DAO-git/src/main/java/com/example/demo/repositories/AgentRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,8 @@ import com.example.demo.entities.Agent;
 
 @Repository
 public interface AgentRepository extends CrudRepository<Agent, String> {
+	List<Agent> findByCommissionGreaterThan(Float commission);
 	
+	@Query("SELECT a FROM Agent a JOIN Customer c ON c.agentCode = a.agentCode")
+	List<Agent> findByWorkingArea(String workingArea);
 }
