93d2de118a755463a4f0b1e933af802707aa9a93
Tom <tom-vautray@hotmail.fr>
2021-10-07 13:55:14 +0200

    Added specific methods in OrderRepository

--- exo-BDD-DAO-git/src/main/java/com/example/demo/repositories/OrderRepository.java
+++ exo-BDD-DAO-git/src/main/java/com/example/demo/repositories/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.example.demo.entities.Order;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Integer> {
+	List<Order> findByAgentCode(String agentCode);
 
+	List<Order> findByOrdAmountGreaterThan(Float orderAmount);
 }
