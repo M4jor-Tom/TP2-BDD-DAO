b0f5743274a03a79a7b363f4fbb2638bff057433
Tom <tom-vautray@hotmail.fr>
2021-10-07 13:54:55 +0200

    Added specific methods in CustomerRepository

--- exo-BDD-DAO-git/src/main/java/com/example/demo/repositories/CustomerRepository.java
+++ exo-BDD-DAO-git/src/main/java/com/example/demo/repositories/CustomerRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import com.example.demo.entities.Customer;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, String> {
-
+	Optional<Customer> findByCustName(String custName);
+	
+	List<Customer> findByCustCity(String custCity);
+	
+	List<Customer> findByOpeningAtmGreaterThan(Float openingAtm);
 }
